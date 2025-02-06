b8385df6d00d4f0a090d9a0e2bec41e537b86a0d
aldi <aldiantodickyu47@gmail.com>
2025-02-06 16:25:42 +0700

    initial commit

---
+++ __init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__, instance_relative_config= True, template_folder='templates')
+app.config.from_object("config")
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+Migrate = Migrate(app, db)
+
+
+from flask_server.app.model import Barang  
+from app.model import customers
+from flask_server.app.model import Pembeli
+from app.model import productnotes
+from flask_server.app.model import Transaksi
+from app import routes
+
